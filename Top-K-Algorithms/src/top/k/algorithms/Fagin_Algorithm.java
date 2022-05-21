1d57d4e602e2df2b358169087302930e8851f42f
Amax <andreas.amaxaris@gmail.com>
2022-05-21 11:44:19 +0300

    just pushing

--- Top-K-Algorithms/src/top/k/algorithms/Fagin_Algorithm.java
+++ Top-K-Algorithms/src/top/k/algorithms/Fagin_Algorithm.java
@@ -5,6 +5,8 @@
  */
 package top.k.algorithms;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -14,6 +16,57 @@ import java.util.List;
 public class Fagin_Algorithm {
 
     public static void Fagin(Integer K, List<SystemAnswer> S1, List<SystemAnswer> S2, List<SystemAnswer> S3) {
+
+        System.out.println("WE ARE IN");
+
+        int Size = S1.size();
+        int lilK = 0;
+
+        List<String> SeenElements = new ArrayList<String>();
+
+        for (int i = 0; i < Size; i++) {
+            /*
+            if (S1.get(i).Element.equals(S2.get(i).Element) && S2.get(i).Element.equals(S3.get(i).Element)) {
+
+                float y = S1.get(i).Score + S2.get(i).Score + S3.get(i).Score;
+                String x = S3.get(i).Element + " " + y;
+                SeenElements.add(x);
+                lilK++;
+                System.out.println("Found?");
+            }*/
+
+            for (int j = 0; j < i; j++) {
+
+                if (S1.get(i).Element.equals(S2.get(j).Element)) {
+
+                    for (int k = 0; k < i; k++) {
+
+                        if (S2.get(j).Element.equals(S3.get(k).Element)) {
+
+                            if (!SeenElements.contains(S3.get(k).Element)) {
+
+                                System.out.println("Found?");
+                                float y = S1.get(i).Score + S2.get(j).Score + S3.get(k).Score;
+                                String x = y + " " + S3.get(k).Element;
+                                SeenElements.add(x);
+                                lilK++;
+                            }
+                        }
+                    }
+
+                }
+
+            }
+
+            if (lilK == K) {
+                break;
+            }
+        }
+
+        Collections.reverse(SeenElements);
+        for (String s : SeenElements) {
+            System.out.println(s);
+        }
 
     }
 }
--- Top-K-Algorithms/src/top/k/algorithms/SystemAnswer.java
+++ Top-K-Algorithms/src/top/k/algorithms/SystemAnswer.java
@@ -13,7 +13,7 @@ import java.util.List;
  */
 public class SystemAnswer {
 
-    char Element;
+    String Element;
     float Score;
 
     /*
@@ -26,79 +26,79 @@ public class SystemAnswer {
 
         /* ---------- System 1 ---------- */
         SystemAnswer E1 = new SystemAnswer();
-        E1.Element = 'E';
+        E1.Element = "E";
         E1.Score = (float) 0.9;
         S1.add(E1);
         SystemAnswer F1 = new SystemAnswer();
-        F1.Element = 'F';
+        F1.Element = "F";
         F1.Score = (float) 0.8;
         S1.add(F1);
         SystemAnswer A1 = new SystemAnswer();
-        A1.Element = 'A';
+        A1.Element = "A";
         A1.Score = (float) 0.8;
         S1.add(A1);
         SystemAnswer B1 = new SystemAnswer();
-        B1.Element = 'B';
+        B1.Element = "B";
         B1.Score = (float) 0.7;
         S1.add(B1);
         SystemAnswer D1 = new SystemAnswer();
-        D1.Element = 'D';
+        D1.Element = "D";
         D1.Score = (float) 0.4;
         S1.add(D1);
         SystemAnswer C1 = new SystemAnswer();
-        C1.Element = 'C';
+        C1.Element = "C";
         C1.Score = (float) 0.2;
         S1.add(C1);
 
         /* ---------- System 2 ---------- */
         SystemAnswer C2 = new SystemAnswer();
-        C2.Element = 'C';
+        C2.Element = "C";
         C2.Score = (float) 0.8;
         S2.add(C2);
         SystemAnswer A2 = new SystemAnswer();
-        A2.Element = 'A';
+        A2.Element = "A";
         A2.Score = (float) 0.7;
         S2.add(A2);
         SystemAnswer F2 = new SystemAnswer();
-        F2.Element = 'F';
+        F2.Element = "F";
         F2.Score = (float) 0.6;
         S2.add(F2);
         SystemAnswer D2 = new SystemAnswer();
-        D2.Element = 'D';
+        D2.Element = "D";
         D2.Score = (float) 0.5;
         S2.add(D2);
         SystemAnswer B2 = new SystemAnswer();
-        B2.Element = 'B';
+        B2.Element = "B";
         B2.Score = (float) 0.5;
         S2.add(B2);
         SystemAnswer E2 = new SystemAnswer();
-        E2.Element = 'E';
+        E2.Element = "E";
         E2.Score = (float) 0.1;
         S2.add(E2);
 
         /* ---------- System 3 ---------- */
         SystemAnswer C3 = new SystemAnswer();
-        C3.Element = 'C';
+        C3.Element = "C";
         C3.Score = (float) 0.7;
         S3.add(C3);
         SystemAnswer A3 = new SystemAnswer();
-        A3.Element = 'A';
+        A3.Element = "A";
         A3.Score = (float) 0.7;
         S3.add(A3);
         SystemAnswer E3 = new SystemAnswer();
-        E3.Element = 'E';
+        E3.Element = "E";
         E3.Score = (float) 0.7;
         S3.add(E3);
         SystemAnswer D3 = new SystemAnswer();
-        D3.Element = 'D';
+        D3.Element = "D";
         D3.Score = (float) 0.4;
         S3.add(D3);
         SystemAnswer F3 = new SystemAnswer();
-        F3.Element = 'F';
+        F3.Element = "F";
         F3.Score = (float) 0.4;
         S3.add(F3);
         SystemAnswer B3 = new SystemAnswer();
-        B3.Element = 'B';
+        B3.Element = "B";
         B3.Score = (float) 0.4;
         S3.add(B3);
 
